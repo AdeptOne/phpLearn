df7f68601ff14bde265081d7c9310fdd3acd74ef
Yesentooky <Yesentooky@gmail.com>
2020-10-04 22:48:33 +0500

    Mastered the function of transferring from 10 to 2 system

--- index.php
+++ index.php
@@ -8,7 +8,9 @@
         <?php
             echo "Вывод рандомного числа от 0 до 2000: ", mt_rand(0,2000) , "<br />";
             $var = 42.43752;
-            echo round($var , 2);
+            echo round($var , 2) . "\n";
+            echo bindec(decbin(4252)) . "\n";
+            echo bindec(decbin(89080));
             class MyClass{
                 //Переменные класса.
             }
