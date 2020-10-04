899a516d446b6f2bce38078980ed90ea9ff5f562
Yesentooky <Yesentooky@gmail.com>
2020-10-04 20:41:59 +0500

    My first commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+            echo "Вывод рандомного числа от 0 до 2000: ", mt_rand(0,2000) , "<br />";
+            $var = 42.43752;
+            echo round($var , 2);
+            class MyClass{
+                //Переменные класса.
+            }
+        ?>
+    </body>
+</html>
